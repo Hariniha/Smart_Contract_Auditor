b756e50942a98a459d07fb498c2ae582b007c65a
Hariniha <harini302004@gmail.com>
2026-05-05 15:18:31 +0530

    .env.exapmle

--- create-docs-dir.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-fs.mkdirSync(
-  'C:\\Users\\prath\\OneDrive\\Documents\\sca\\Smart_Contract_Auditor\\src\\app\\documentation',
-  { recursive: true },
-);
-console.log('Directory created successfully');
